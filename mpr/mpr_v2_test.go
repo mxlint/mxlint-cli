be9e9ecd23e18226f062a924f14b08501fcc3948
Xiwen Cheng <x@cinaq.com>
2025-02-09 22:08:17 +0200

    Support mpr V2

---
+++ mpr/mpr_v2_test.go
@@ -0,0 +1,42 @@
+// mpr_test.go
+package mpr
+
+import (
+	"os"
+	"testing"
+
+	"gopkg.in/yaml.v2"
+)
+
+// TestAdd tests the Add function to ensure it returns correct results.
+func TestMPRV2Metadata(t *testing.T) {
+	t.Run("single-mpr", func(t *testing.T) {
+		if err := exportMetadata("./../resources/app-mpr-v2", "./../tmp"); err != nil {
+			t.Errorf("Failed to export metadata from MPR file")
+		}
+
+		// open metadata file
+		metadataFile, err := os.ReadFile("./../tmp/Metadata.yaml")
+		if err != nil {
+			t.Errorf("Failed to read metadata file")
+		}
+		// read metadata file
+		var metadataObj MxMetadata
+		if err := yaml.Unmarshal(metadataFile, &metadataObj); err != nil {
+			t.Errorf("Failed to unmarshal metadata file")
+		}
+		// check metadata
+		expectedProductVersion := "10.18.2.58475"
+		if metadataObj.ProductVersion != expectedProductVersion {
+			t.Errorf("ProductVersion is incorrect. Expected: %s, Got: %s", expectedProductVersion, metadataObj.ProductVersion)
+		}
+	})
+}
+
+func TestMPRV2Units(t *testing.T) {
+	t.Run("single-mpr", func(t *testing.T) {
+		if err := exportUnits("./../resources/app-mpr-v2", "./../tmp", false, "basic"); err != nil {
+			t.Errorf("Failed to export units from MPR file")
+		}
+	})
+}
