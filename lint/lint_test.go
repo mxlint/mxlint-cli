ed403324572d3ba47902aae84b9cb3295bbde84f
Xiwen Cheng <x@cinaq.com>
2025-05-20 20:33:01 +0200

    move modelsource directories into resources

--- lint/lint_test.go
+++ lint/lint_test.go
@@ -7,7 +7,7 @@ import (
 // TestAdd tests the Add function to ensure it returns correct results.
 func TestLintSingle(t *testing.T) {
 	// t.Run("single policy skipped", func(t *testing.T) {
-	// 	result, err := evalTestsuite("./../policies/001_project_settings/001_0004_strong_password.rego", "./../modelsource")
+	// 	result, err := evalTestsuite("./../policies/001_project_settings/001_0004_strong_password.rego", "./../resources/modelsource")
 
 	// 	if err != nil {
 	// 		t.Errorf("Failed to evaluate")
@@ -19,7 +19,7 @@ func TestLintSingle(t *testing.T) {
 	// })
 	t.Run("single Rego rule passes", func(t *testing.T) {
 		rule, _ := parseRuleMetadata_Rego("./../resources/rules/001_0003_security_checks.rego")
-		result, err := evalTestsuite(*rule, "./../modelsource")
+		result, err := evalTestsuite(*rule, "./../resources/modelsource")
 
 		if err != nil {
 			t.Errorf("Failed to evaluate")
@@ -31,7 +31,7 @@ func TestLintSingle(t *testing.T) {
 	})
 	t.Run("single JS rule passes", func(t *testing.T) {
 		rule, _ := parseRuleMetadata_Javascript("./../resources/rules/001_0002_demo_users_disabled.js")
-		result, err := evalTestsuite(*rule, "./../modelsource")
+		result, err := evalTestsuite(*rule, "./../resources/modelsource")
 
 		if err != nil {
 			t.Errorf("Failed to evaluate")
@@ -45,7 +45,7 @@ func TestLintSingle(t *testing.T) {
 
 func TestLintBundle(t *testing.T) {
 	t.Run("all-rules", func(t *testing.T) {
-		err := EvalAll("./../resources/rules", "./../modelsource", "", "")
+		err := EvalAll("./../resources/rules", "./../resources/modelsource", "", "")
 
 		if err != nil {
 			t.Errorf("No failures expected: %v", err)
