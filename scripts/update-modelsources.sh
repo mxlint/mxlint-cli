7ff3f0af824aeb57766377d0292e7996f9bfcdbf
Xiwen Cheng <x@cinaq.com>
2025-05-23 17:08:08 +0200

    Refresh resources

--- scripts/update-modelsources.sh
+++ scripts/update-modelsources.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 #
 #
-./bin/mxlint-darwin-arm64 export-model -i resources/app-mpr-v1
+./bin/mxlint-darwin-arm64 export-model -i resources/app-mpr-v1 -o resources/modelsource-v1
+./bin/mxlint-darwin-arm64 export-model -i resources/app-mpr-v2 -o resources/modelsource-v2
